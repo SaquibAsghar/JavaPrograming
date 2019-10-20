61a56af0f732ec0704ef2051aa8f7b41b9e376c3
SaquibAsghar <saq9516@gmail.com>
2019-10-20 19:09:14 +0530

    updated HelloWorld

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int myFirstNumber = 10+2;
+        System.out.println(myFirstNumber);
     }
 }
