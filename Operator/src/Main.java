7222ba4479262812c7ea3a3297312d2277c8eaf7
SaquibAsghar <saq9516@gmail.com>
2019-10-21 14:09:58 +0530

    Create Main.java

---
+++ Operator/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 10 + 20;
+        // ' // ' is a single line comment
+        // ' = ' is a assignment operator
+        System.out.println("The result of 10 + 20 :"+result);
+        result = result * 10;
+        System.out.println("result of ( 10+20 )*10 :"+result);
+        result = result / 10;
+        System.out.println("result of ( 10+20 )/10 :"+result);
+        int resultRemainder = result % 10; // this will give us the remainder
+        System.out.println("remainder of ( 10+20 )/10 :"+resultRemainder);
+        
+    }
+}
