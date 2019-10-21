c07a7c202aacf4ff9ac16ac85c4213fbc2e17d2d
SaquibAsghar <saq9516@gmail.com>
2019-10-21 13:35:29 +0530

    created char and boolean file

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println("Char:"+myChar);
+        System.out.println("UnicodeCharacter:"+myUnicodeChar);
+       boolean myTrueBoolean = true;
+       boolean myFalseBoolean = false;
+       if(myTrueBoolean)
+           System.out.println("true it is");
+
+    }
+}
