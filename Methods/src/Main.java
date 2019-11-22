2951985a7b2914a68c1520d59af6c1417cdf03ed
SaquibAsghar <saq9516@gmail.com>
2019-11-22 14:27:22 +0530

    Update Main.java

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -49,14 +49,26 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore){
+//        if(playerScore >= 1000){
+//            return 1;
+//        }else if (playerScore >= 500 ) {
+//            return 2;
+//        }else if(playerScore >= 100){
+//            return 3;
+//        }
+//            return 4;
+
+
+        // alternative way
+
+        int position= 4;
         if(playerScore >= 1000){
-            return 1;
+            position= 1;
         }else if (playerScore >= 500 ) {
-            return 2;
+            position= 2;
         }else if(playerScore >= 100){
-            return 3;
-        }else{
-            return 4;
+            position= 3;
         }
+        return position;
     }
 }
