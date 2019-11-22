07ec28e57dcb61172e006306ed7aeb7f85d9aa31
SaquibAsghar <saq9516@gmail.com>
2019-11-22 11:33:17 +0530

    Create Main.java

---
+++ StatementsWhitespaceAndIndenting/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        System.out.println("This is an Expression");
+        System.out.println("This is " +
+                "another " +
+                "still more"
+        );
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+
+
+    }
+}
