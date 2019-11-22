34ff028f95b1d6c937e66852fb34befe6c903ce4
SaquibAsghar <saq9516@gmail.com>
2019-11-22 11:32:57 +0530

    Create Main.java

---
+++ KeywordsAndExpression/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers
+        int mile = 100;
+        double kilometers = (mile * 1.6);
+        System.out.println( mile+" miles= "+ kilometers + " Kilometers");
+        int highScore = 50;
+        if (highScore == 50 ){
+            System.out.println("This is an expression");
+        }
+
+        int score = 100;
+        if (score > 99){
+            System.out.println("You got high score");
+            score = 0;
+        }
+    }
+}
